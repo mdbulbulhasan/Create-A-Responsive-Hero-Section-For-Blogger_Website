ec8154d370eaf12a9eebcda9e794b6658a292879
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-09-16 19:20:40 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector(".menu-button");
+
+const mobileMenuItems = document.querySelector(".mobile-menu-items");
+
+menuButton.addEventListener("click", () => {
+    mobileMenuItems.classList.toggle("active");
+})
